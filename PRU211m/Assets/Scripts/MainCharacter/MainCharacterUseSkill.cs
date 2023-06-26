bb6b113da435677df5cfd595d87e3ad253a2c1d3
Danh Lê <danhltse161278@fpt.edu.vn>
2023-06-26 11:02:13 +0700

    232606_CastSkill_SingleSkill_Animation_Code
    Cast skill animation
    Skill animation
    Cast skill mechanism
    Single skill
    ...like sh*t

---
+++ PRU211m/Assets/Scripts/MainCharacter/MainCharacterUseSkill.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacterUseSkill : MonoBehaviour
+{
+    [SerializeField]private float useSkillCooldown;
+    [SerializeField] private Transform firepoint;
+    [SerializeField] private GameObject[] fireballs; 
+    private Animator anim;
+    private MainCharacterMovement mainCharacterMovement;
+    private float cooldownTimer = Mathf.Infinity;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        mainCharacterMovement = GetComponent<MainCharacterMovement>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1) && cooldownTimer > useSkillCooldown && mainCharacterMovement.CanAttack())
+        {
+            CastSkill();
+        }
+        else
+        {
+            anim.ResetTrigger("Cast");
+        }
+
+        cooldownTimer += Time.deltaTime;
+    }
+
+    private void CastSkill()
+    {
+        anim.SetTrigger("Cast");
+        cooldownTimer = 0;
+
+        fireballs[FindSkillProjecttile()].transform.position = firepoint.position;
+        fireballs[FindSkillProjecttile()].GetComponent<Pojectie>().SetDirection(Mathf.Sign(transform.localScale.x));
+    }
+
+    private int FindSkillProjecttile()
+    {
+        for(int i = 0; i < fireballs.Length; i++)
+        {
+            if (!fireballs[i].activeInHierarchy)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+}
