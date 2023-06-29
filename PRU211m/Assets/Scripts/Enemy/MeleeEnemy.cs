9a08c09cf74aaaa122377bc145b0fd2fc1d43ae9
PhatBaka <nguyenquyphat2711@gmail.com>
2023-06-30 05:58:18 +0700

    create enemy

--- PRU211m/Assets/Scripts/Enemy/MeleeEnemy.cs
+++ PRU211m/Assets/Scripts/Enemy/MeleeEnemy.cs
@@ -2,16 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyAttack : MonoBehaviour
+public class MeleeEnemy : MonoBehaviour
 {
     [Header("Attack Parameters")]
     [SerializeField] private float attackCooldown;
     [SerializeField] private float range;
     [SerializeField] private int damage;
-
-    [Header("Ranged Attack")]
-    [SerializeField] private Transform firepoint;
-    [SerializeField] private GameObject[] fireballs;
 
     [Header("Collider Parameters")]
     [SerializeField] private float colliderDistance;
@@ -20,48 +16,46 @@ public class EnemyAttack : MonoBehaviour
     [Header("Player Layer")]
     [SerializeField] private LayerMask playerLayer;
     private float cooldownTimer = Mathf.Infinity;
-    [Header("Dialogue")]
-    [SerializeField] private GameObject dialogue;
-    [Header("Fireball Sound")]
-
-    [SerializeField] private AudioClip fireballSound;
-    
 
     //References
     private Animator anim;
-    private bool flag = false;
+    private Heart playerHealth;
+    private EnemyPatrol enemyPatrol;
+
     private void Awake()
     {
-        anim = GetComponent<Animator>();    
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        anim = GetComponent<Animator>();
+        enemyPatrol = GetComponentInParent<EnemyPatrol>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        cooldownTimer += Time.deltaTime;
+
+        //Attack only when player in sight?
         if (PlayerInSight())
         {
-            //if (!DialogueLine.flagCheckDialog)
-            //{
-            //    dialogue.active = true;
-            //}
             if (cooldownTimer >= attackCooldown)
             {
                 cooldownTimer = 0;
                 anim.SetTrigger("meleeAttack");
             }
         }
+
+        if (enemyPatrol != null)
+            enemyPatrol.enabled = !PlayerInSight();
     }
+
     private bool PlayerInSight()
     {
         RaycastHit2D hit =
-            Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * -transform.localScale.x * colliderDistance,
+            Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance,
             new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z),
             0, Vector2.left, 0, playerLayer);
+
+        if (hit.collider != null)
+            playerHealth = hit.transform.GetComponent<Heart>();
+
         return hit.collider != null;
     }
     private void OnDrawGizmos()
@@ -71,5 +65,9 @@ public class EnemyAttack : MonoBehaviour
             new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
     }
 
-
+    private void DamagePlayer()
+    {
+        if (PlayerInSight())
+            playerHealth.TakeDamage(damage);
+    }
 }
