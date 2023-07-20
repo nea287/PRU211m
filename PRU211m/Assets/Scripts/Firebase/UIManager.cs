26a4d27b5df18ead194fc39033860940812bd878
PhatBaka <nguyenquyphat2711@gmail.com>
2023-07-20 23:40:46 +0700

    feat: firebase authen done, firebase realtime in progress

---
+++ PRU211m/Assets/Scripts/Firebase/UIManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    //Screen object variables
+    public GameObject loginUI, registerUI, warningUI;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != null)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    //Functions to change the login screen UI
+    public void LoginScreen() //Back button
+    {
+        loginUI.SetActive(true);
+        registerUI.SetActive(false);
+    }
+    public void RegisterScreen() // Regester button
+    {
+        loginUI.SetActive(false);
+        registerUI.SetActive(true);
+    }
+    public void WarningScreen()
+    {
+        warningUI.SetActive(true);
+    }
+}
